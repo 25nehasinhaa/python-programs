c83662ac58e7b25796eb510ae2884e949d87acb0
25nehasinhaa <48133174+25nehasinhaa@users.noreply.github.com>
2019-03-07 10:19:11 +0530

    Add files via upload

--- modules.py
+++ modules.py
@@ -4,4 +4,10 @@ def add(a,b):
 	return(a+b)
 
 def sub(a,b):
-	return(a-b)	
+	return(a-b)
+
+def mult(a,b):
+	return(a*b)
+
+def div(a,b):
+	return(a/b)	
--- prac.py
+++ prac.py
@@ -1,1 +1,12 @@
 import modules
+c=modules.add(5,8)
+print(c)
+
+c=modules.sub(5,2)
+print(c)
+
+c=modules.mult(4,2)
+print(c)
+
+c=modules.div(10,5)
+print(c)
